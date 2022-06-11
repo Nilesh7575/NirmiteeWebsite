ba97562dff2881f68a56b6acc121875759c19268
Nilesh Mistary <nilesh.mistary@nirmitee.io>
2022-06-11 15:14:21 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Cover from "./components/Cover";
 import Services from "./components/Services";
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <Cover/>
       <Services />
     </div>
   );
---
+++ src/components/Cover.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './cover.css'
+
+
+
+
+const Cover = () => {
+    return (
+        <div className='banner'>
+            <div className='container'>
+                <div className='headline'>
+                    <h1>Understand Business and Deliver Technology</h1>
+                    <h3>Our Business consulting experts understand your business, just like you,
+                        and help us develop a more relevant version of your requirements,
+                        adding a cutting edge to our deliveries</h3>
+                    <div className='button'>
+                    <a class="btn" href="/" >TALK TO OUR EXPERT</a>
+                    <a class="btn" href="/" >VIEW OUR PORTFOLIO</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cover
