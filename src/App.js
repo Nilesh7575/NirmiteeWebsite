9ed928f0bce77d23fd2b62ec39a7931b7f5cb54e
Abhinav <abhinavagwl5@gmail.com>
2022-06-11 18:49:29 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import Cover from "./components/Cover";
 import Services from "./components/Services";
-
-
-
-
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Cover/>
-      <Services />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Cover />
+            <Services />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
